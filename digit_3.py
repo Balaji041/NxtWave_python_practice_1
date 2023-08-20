6e63a9cc4219f990ffcdf0e6aa6e7d856975b7ce
balaji <76476394+Balaji041@users.noreply.github.com>
2023-08-20 13:58:08 +0530

    Update digit_3.py

--- digit_3.py
+++ digit_3.py
@@ -1,6 +1,6 @@
 N=int(input())
 for i in range(1,2*N):
-    if i==1 or i==2*N-1 or i==(N*2)//2:
+    if i==1 or i==2*N-1 or i==N:
         print("* "*N)
     elif (N*2)//2:
         print("  "*(N-1)+"* ")
